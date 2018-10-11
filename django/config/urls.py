f5b3a78e8d2a7adeae34b28c043f1968f30b44bd
Dmytriy Myk <porosya@gmail.com>
2018-10-11 04:39:02 +0300

    add user app

--- django/config/urls.py
+++ django/config/urls.py
@@ -13,12 +13,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import include, path
 
 import core.urls
+import user.urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('user/', include(user.urls, namespace='user')),
     path('', include(core.urls, namespace='core')),
+
 ]
--- django/core/views.py
+++ django/core/views.py
@@ -1,9 +1,9 @@
-from django.shortcuts import render
+# from django.shortcuts import render
 
 from django.views.generic import (
     ListView, DetailView,)
 
-from core.models import Movie
+from core.models import Movie, Person
 
 
 class MovieList(ListView):
@@ -12,4 +12,8 @@ class MovieList(ListView):
 
 
 class MovieDetail(DetailView):
-    model = Movie
+    queryset = (Movie.objects.all_with_related_persons())
+
+
+class PersonDetail(DetailView):
+    queryset = Person.objects.all_with_prefetch_movies()
